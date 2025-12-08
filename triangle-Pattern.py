f6b412a3e33c7a01934c252f83bd9fe5b397c7f3
sneha vanani <vananisneha0@gmail.com>
2025-12-08 15:06:12 +0530

    questions added

--- triangle-Pattern.py
+++ triangle-Pattern.py
@@ -138,3 +138,107 @@ for i in range(n):
    for j in range(n-i):
         print('*',end='')
    print()
+
+"""   1
+     121
+    12321
+   1234321"""
+n = int(input("Enter a number: "))
+
+for i in range(n):
+   for s in range(n-i+1):
+       print(end=' ')
+
+   for j in range(i+1):
+        print((j+1),end='')
+
+   for k in range(i):
+       print((i-k), end='')
+   print()
+
+"""dabangg pattern :-
+1234554321
+1234**4321
+123****321
+12******21
+1********1"""
+n = int(input("Enter a number: "))
+
+for i in range(n):
+   for j in range(n-i):
+        print(j+1,end=' ')
+   
+   for s in range(i):
+       print('* *',end=' ')
+
+   for k in range(n-i):
+       print((n-i-k),end=" ")
+   print()
+
+"""  *
+    * *
+   * * *       
+    * *
+     * """
+n = int(input("Enter a number: "))
+
+for i in range(1,n+1):
+   for s in range(n - i):
+         print(end=' ')
+
+   for j in range(i):
+        print('*',end=' ')
+   print()
+
+for i in range(i-1, 0, -1):
+   for s in range(n - i):
+      print(end=' ')
+
+   for j in range(i):
+      print('*',end=' ')
+   print()
+    
+"""
+*********
+ *******
+  *****
+   ***
+    * """
+n = int(input("enter a number: "))
+
+for i in range(n):
+   for s in range(i):
+      print(end=' ')
+
+   for j in range(n-i):
+      print('*',end='')
+
+   for k in range(n - (i +1)):
+      print('*',end='')
+   print()
+
+"""   
+   *
+  **
+ ***
+****
+ ***
+  **
+   * """
+n = int(input("enter a number: "))
+
+for i in range(n):
+   for s in range(n-(i+1)):
+      print(end=' ')
+
+   for j in range(i+1):
+      print('*',end='')
+   print()
+
+for k in range(i, 0, -1):
+   for s in range(n-k):
+      print(end=' ')
+
+   for j in range(k):
+      print('*',end='')
+   print()
