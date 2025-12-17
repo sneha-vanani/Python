26a7801943bdc976b0808440dff2aec91954c5ae
sneha vanani <vananisneha0@gmail.com>
2025-12-17 17:41:38 +0530

    new logic added

--- List/Second_Greatest.py
+++ List/Second_Greatest.py
@@ -1,14 +1,20 @@
 #Find the second greates elemnt 
 num = [34,56,79,23,1,29,70]
-greatest = num[0]
-second_great = num[0]
 
-for i in num:
-    if i > greatest:
-        second_great = greatest
-        greatest = i
-    elif i > second_great:
-        second_great = i
+""" Older logic"""
+# greatest = num[0]
+# second_great = num[0]
+
+# for i in num:
+#     if i > greatest:
+#         second_great = greatest
+#         greatest = i
+#     elif i > second_great:
+#         second_great = i
         
+# print(f"Greatest: {greatest} \nthe second greatest elemnt : {second_great}")
 
-print(f"Greatest: {greatest} \nthe second greatest elemnt : {second_great}")
+"""new logic"""
+greatest = max(num)
+second_great = max(x for x in num if x != greatest)
+print(f"Greatest: {greatest} \nthe second greatest element : {second_great}")
