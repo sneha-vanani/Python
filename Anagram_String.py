295e3e44a51c9e21f5711f17e7549532de731db9
sneha vanani <vananisneha0@gmail.com>
2026-01-12 21:06:03 +0530

    initial file

--- Anagram_String.py
+++ Anagram_String.py
@@ -7,15 +7,27 @@ Both strings have the same length
 
 Both strings have the same characters with the same frequency"""
 
+# Old logic
+
+# def anagram(s1,s2):
+#     if len(s1) == len(s2):
+#         for i in s1:
+#             if i in s2:
+#                 continue
+#             else:
+#                 return False
+#         else:
+#             return True
+
 def anagram(s1,s2):
-    if len(s1) == len(s2):
-        for i in s1:
-            if i in s2:
-                continue
-            else:
-                return False
-        else:
-            return True
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+
+
+    if len(s1) != len(s2):
+        return False
+    
+    return sorted(s1) == sorted(s2)
 
 
 s1 = input('enter first string: ')
